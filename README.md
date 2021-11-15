a70ac7e72e90419a059bdcd67ea4bec21fee4988
gisellyreis <gisellyareis@gmail.com>
2021-11-15 13:24:58 -0300

    get data from mqtt

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ obs: sc01 é o nome do device/sensor e o ip do servidor/gateway é 192.168.0.129
 
 Passo 2 - Enviar mensagem solicitando que o sensor comece a publicar amostras:
 
-mosquitto_pub -t 'dev/sc01' -h '10.0.0.106' -m '{"method":"flow", "sensor":"temperatureSensor", "time":{"collect":50000,"publish":50000}}'
+mosquitto_pub -t 'dev/sc01' -h '10.0.0.106' -m '{"method":"GET", "sensor":"temperatureSensor"}'
+
+mosquitto_pub -t 'dev/sc01' -h '10.0.0.106' -m '{"method":"flow", "sensor":"temperatureSensor", "time":{"collect":10000,"publish":10000}}'
+
+mosquitto_pub -t 'dev/sc02' -h '10.0.0.106' -m '{"method":"flow", "sensor":"humiditySensor", "time":{"collect":10000,"publish":10000}}'
 OBS: Iniciando a publicação de amostras do dispositivo sc01 sensor de radiação solar.
 
 Passo 3 - Verificar se as mensagens estão sendo enviadas
--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 console.log('hi');
 
-function getTemperature() {
-    return fetch("temperature.json")
-    .then(response => response.json())
-    .then((data) => {
-        console.log(data);
-        var length = data.length -1;
-        document.getElementById("info").innerHTML = "";
-        var node = document.createElement("p");
-        var text = document.createTextNode("Temperatura: "+ data[length].data[0]);
-        node.appendChild(text);;
-
-        document.getElementById("info").appendChild(node);
-    });
+async function getTemperature() {
+    const response = await fetch("temperature.json");
+    const data = await response.json();
+    console.log(data);
+    var length = data.length - 1;
+    document.getElementById("info").innerHTML = "";
+    var node = document.createElement("p");
+    var text = document.createTextNode("Temperatura: " + data[length].data[0]);
+    node.appendChild(text);
+    
+    document.getElementById("info").appendChild(node);
 }
 
 
@@ -27,4 +25,14 @@ window.onload = () => {
     document.getElementById("body").appendChild(btn);
 }
 
+function btnAgradavel() {
+    console.log('agradavel');
+}
 
+function btnQuente() {
+    console.log('quente');
+}
+
+function btnFrio() {
+    console.log('frio');
+}
