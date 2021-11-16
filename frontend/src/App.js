5cf486899c6d8f9ca19481d948fc4e9b1329f15a
gisellyreis <gisellyar@ufba.br>
2021-11-16 00:10:42 -0300

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,72 +3,49 @@ import * as d3 from 'd3';
 import './App.css';
 import api from './services/api';
 
+import temperatureImg from './assets/temperatura.png';
+import humidityImg from './assets/umidade.png';
+
 function App() {
-  const [data, setData] = useState([]);
-  const [dataTeste] = useState([]);
-  const svgRef = useRef();
+  const [temperatureData, setTemperatureData] = useState([]);
+  const [humidityData, setHumidityData] = useState([]);
 
-
-  function teste() {
-    data.forEach(ele => {
-      dataTeste.push(parseInt(ele.data[0]));
-    })
-
-    console.log(dataTeste);
-  }
-
-  useEffect(() => {
+  /* useEffect(() => {
     api.get('sensor?sensor=temperatureSensor')
-       .then(res => {
-         console.log(res.data);
-         setData(res.data);
-       })
+      .then(res => {
+        setTemperatureData(res.data);
+      })
 
+    api.get('sensor?sensor=humiditySensor')
+    .then(res => {
+      setHumidityData(res.data);
+    })
+  }) */
 
-    // set up svg
-    const w = 800;
-    const h = 400;
-    const svg = d3.select(svgRef.current)
-                  .attr('width', w)
-                  .attr('heigth', h)
-                  .style('background', '#d3d3d3')
-                  .style('margin-top', '50')
-                  .style('overflow', 'visible');
-    // set up scalling
-    const xScale = d3.scaleLinear()
-                     .domain([0, dataTeste.length -1])
-                     .range([0, w])
-    const yScale = d3.scaleLinear()
-                     .domain([0, h])
-                     .range([h, 0])
-    const generateScaledLine = d3.line()
-          .x((d, i) => xScale(i))
-          .y(yScale)
-          .curve(d3.curveCardinal);
-    // set the axes
-    const xAxis = d3.axisBottom(xScale)
-                    .ticks(dataTeste.length/10)
-                    .tickFormat(i => i+1);
-    const yAxis = d3.axisLeft(yScale)
-                    .ticks(5);
-    svg.append('g')
-       .call(xAxis)
-       .attr('transform', `translate(0, ${h})`);
-    svg.append('g')
-       .call(yAxis);
-    // set up data for svg
-    svg.selectAll('.line')
-       .data([dataTeste])
-       .join('path')
-            .attr('d', d => generateScaledLine(d))
-            .attr('fill', 'none')
-            .attr('stroke', 'black')
-  }, []);
- 
+  
+
   return (
     <div className="App">
-      {teste()}
-      <svg ref={svgRef}></svg>
+        <div className="group">
+          <div className="temperature">
+            <img src={temperatureImg} />
+            <strong>Temperatura:</strong>
+            <strong>26ºC</strong>
+          </div>
+          <div className="humidity">
+            <img src={humidityImg} />
+            <strong>Umidade:</strong>
+            <strong>100%</strong>
+          </div>
+        </div>
+        <div className="feedback">
+          <p>Você acha que a sala está: </p>
+          <div className="btn-group">
+            <button>Agradável</button>
+            <button>Quente</button>
+            <button>Fria</button>
+          </div>
+        </div>
     </div>
   );
 }
